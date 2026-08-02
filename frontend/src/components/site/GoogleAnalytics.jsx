be14edd7d6076e74968b04f58080edfefd06f69c
ashish <ashishyadav.work25@gmail.com>
2026-08-02 15:54:39 +0530

    Create GoogleAnalytics.jsx

---
+++ frontend/src/components/site/GoogleAnalytics.jsx
@@ -0,0 +1,23 @@
+import Script from "next/script";
+
+const GA_MEASUREMENT_ID = "G-EED0P3WZSV";
+
+export default function GoogleAnalytics() {
+  return (
+    <>
+      <Script
+        id="gtag-src"
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
+      />
+      <Script id="gtag-init" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${GA_MEASUREMENT_ID}');
+        `}
+      </Script>
+    </>
+  );
+}
