29ae3b9f391a3985e26321c7e71f6bbee6446989
Ashish <ashishyadav.work25@gmail.com>
2026-09-08 01:17:47 +0530

    Update CategoryListClient.jsx

--- frontend/src/components/site/CategoryListClient.jsx
+++ frontend/src/components/site/CategoryListClient.jsx
@@ -3,6 +3,7 @@
 import { useMemo } from "react";
 import { useSearchParams } from "next/navigation";
 import PostCard from "@/components/site/PostCard";
+import AdSlot from "@/components/site/AdSlot";
 import FilterBar from "@/components/site/FilterBar";
 import { filterPosts } from "@/lib/postFilters";
 import { Info } from "lucide-react";
